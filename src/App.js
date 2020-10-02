805f3beefea5f85426bfdba7f70e2da9bb0356b9
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-10-02 14:30:49 -0400

    comp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+    constructor() {
+    super();
+    this.state = {...this.getInitialState(), circleSelected: '1'};
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
-        <main>YOUR TWO COMPONENTS WILL GO HERE</main>
+
       </div>
     );
   }
