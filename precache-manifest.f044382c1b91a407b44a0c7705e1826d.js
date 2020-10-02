ec3ba85e8d7358442427480a6650a0b9326c5a3e
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-10-02 18:04:43 -0400

    Updates

---
+++ precache-manifest.f044382c1b91a407b44a0c7705e1826d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac9cbbf39a2ba2a6f89179113ff774d",
+    "url": "/Assessment-4-joe/index.html"
+  },
+  {
+    "revision": "ce32249ffa980c143713",
+    "url": "/Assessment-4-joe/static/css/main.ac0de4b8.chunk.css"
+  },
+  {
+    "revision": "9886b3704be02d7489c9",
+    "url": "/Assessment-4-joe/static/js/2.5946832a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assessment-4-joe/static/js/2.5946832a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce32249ffa980c143713",
+    "url": "/Assessment-4-joe/static/js/main.10e5942a.chunk.js"
+  },
+  {
+    "revision": "dcebe9f65fd19d6c8433",
+    "url": "/Assessment-4-joe/static/js/runtime-main.f09b7172.js"
+  }
+]);
